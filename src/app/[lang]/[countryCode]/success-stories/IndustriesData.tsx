049ed5dcd2783ba6abf21ed5709fe646e2ab616c
Kousik S <kousik.s@bytebandits.in>
2025-12-04 19:51:13 +0530

    slug

---
+++ src/app/[lang]/[countryCode]/success-stories/IndustriesData.tsx
@@ -0,0 +1,100 @@
+const IndustriesData = [
+  {
+    title: 'Insurance',
+    image: '/images/industries/insurance.webp',
+  },
+  {
+    title: 'Automobile',
+    image: '/images/industries/automobile.webp',
+  },
+  {
+    title: 'Pharmacy',
+    image: '/images/industries/pharmacy.webp',
+  },
+  {
+    title: 'Energy',
+    image: '/images/industries/energy.webp',
+  },
+  {
+    title: 'Packaging Solution',
+    image: '/images/industries/packaging-solution.webp',
+  },
+  {
+    title: 'Education',
+    image: '/images/industries/education.webp',
+  },
+  {
+    title: 'Construction & Engineering',
+    image: '/images/industries/construction-engineering.webp',
+  },
+  {
+    title: 'Logistics',
+    image: '/images/industries/logistics.webp',
+  },
+  {
+    title: 'Oil & Gas',
+    image: '/images/industries/oil-and-gas.webp',
+  },
+  {
+    title: 'FMCD',
+    image: '/images/industries/fmcd.webp',
+  },
+  {
+    title: 'Hospitality and Tourism',
+    image: '/images/industries/hospitality-and-tourism.webp',
+  },
+  {
+    title: 'Beauty & Wellness',
+    image: '/images/industries/beauty-and-wellness.webp',
+  },
+  {
+    title: 'Trading',
+    image: '/images/industries/trading.webp',
+  },
+  {
+    title: 'Telecom',
+    image: '/images/industries/telecom.webp',
+  },
+  {
+    title: 'ICT',
+    image: '/images/industries/ict.webp',
+  },
+  {
+    title: 'Chemicals',
+    image: '/images/industries/chemicals.webp',
+  },
+  {
+    title: 'Food & Beverage',
+    image: '/images/industries/food-and-beverage.webp',
+  },
+  {
+    title: 'Fashion',
+    image: '/images/industries/fashion.webp',
+  },
+  {
+    title: 'Ecommerce',
+    image: '/images/industries/ecommerce.webp',
+  },
+  {
+    title: 'Retail',
+    image: '/images/industries/retail.webp',
+  },
+  {
+    title: 'Aerospace & Defense',
+    image: '/images/industries/aerospace-defense.webp',
+  },
+  {
+    title: 'Electronics',
+    image: '/images/industries/electronics.webp',
+  },
+  {
+    title: 'Manufacturing',
+    image: '/images/industries/manufacturing.webp',
+  },
+  {
+    title: 'Home appliances',
+    image: '/images/industries/home-appliances.webp',
+  },
+];
+
+export default IndustriesData;
