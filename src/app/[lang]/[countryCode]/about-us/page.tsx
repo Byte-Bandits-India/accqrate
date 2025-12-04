c058b95bbfffdb3d74c149dd8f47174bc53973a6
Kousik S <kousik.s@bytebandits.in>
2025-12-04 19:27:48 +0530

    Delete src/app directory

--- src/app/[lang]/[countryCode]/about-us/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import ComingSoon from '@/components/CommingSoon'
-
-export default function About() {
-  return <ComingSoon />
-}
--- src/app/[lang]/[countryCode]/coming-soon/page.tsx
+++
@@ -1,5 +0,0 @@
-import ComingSoon from '@/components/CommingSoon'
-
-export default function ComingSoonPage() {
-    return <ComingSoon />
-}
