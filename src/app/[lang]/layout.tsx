ec4a3cb1eedc092eedc03ec07be4367a32eb8863
Kousik S <kousik.s@bytebandits.in>
2025-12-05 18:59:11 +0530

    Update layout.tsx

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -8,7 +8,7 @@ interface LangLayoutProps {
     }>;
 }
 
-const supportedLanguages = ['en', 'ar'];
+const supportedLanguages = ['en', 'ar', 'ml', 'fr', 'nl'];
 
 export default async function LangLayout({ children, params }: LangLayoutProps) {
     const { lang } = await params;
