26a90f56e53295c057bc60f3e225fe128de490c1
Kousik-Bytebandits <kousik.bbtech@gmail.com>
2025-12-25 14:32:29 +0530

    e-invoice

---
+++ src/app/[lang]/[countryCode]/e-invocing/integration/data/CountryContent.ts
@@ -0,0 +1,60 @@
+import AssetPath from "@/AssetPath/AssetPath";
+
+export interface CountryContent {
+  title: string;
+  titlend : string;
+  highlight: string;
+  subtitle: string;
+  boxTitle: string;
+  boxSubtitle: string;
+  boxImage: string;
+  highlightColor: string;
+}
+
+export const COUNTRY_CONTENT: Record<string, CountryContent> = {
+  sa: {
+    title: "Integrate Your Existing ERPs, POS & Applications With",
+    titlend: "",
+    highlight: "ZATCA",
+    subtitle: "E-Invoicing Integration",
+    boxTitle: "A ZATCA-Approved Solution Provider",
+    boxSubtitle: "100% ZATCA Compliance: Saudi Based Solution For Saudi Businesses",
+    boxImage: AssetPath.invoicing.integrationAssets.zatca.src,
+    highlightColor: "#1c2041"
+  },
+
+  be: {
+    title: "Integrate Your Existing ERPs, POS & Business Applications With Belgium's",
+    titlend: "",
+    highlight: "Peppol Network",
+    subtitle: "PEPPOL E-INVOICING INTEGRATION",
+    boxTitle: "A Peppol-Ready EN 16931-Compliant Solution",
+    boxSubtitle: "100% Compliance For Belgium's 2026  B2B E-Invoicing Mandate",
+    boxImage: AssetPath.integration.peppol.src,
+    highlightColor: "#1c2041"
+  },
+
+  ae: {
+    title: "Integrate Your Existing",
+    titlend: "",
+    highlight: "ERPs, POS & Applications With FTA (UAE PINT)",
+    subtitle: "FTA E-INVOICING INTEGRATION",
+    boxTitle: "A Peppol Certified - UAE PINT, 5 Corner Model Ready Solution",
+    boxSubtitle: "100% Compliance for UAE's E-Invoicing.",
+    boxImage: AssetPath.integration.peppol.src,
+    highlightColor: "#1c2041"
+  },
+
+  pl: {
+    title: "Connect Your ERPs, POS & Applications Seamlessly With ",
+    titlend: "",
+    highlight: "KSeF",
+    subtitle: "Peppol E-Invoicing Integration",
+    boxTitle: "A Fully KSeF-Compatible, FA(3) XML-Ready Platform",
+    boxSubtitle: "100% Compliance For Poland's Mandatory E-Invoicing Framework",
+    boxImage: AssetPath.integration.peppol.src,
+    highlightColor: "#1c2041"
+  },
+
+
+};
